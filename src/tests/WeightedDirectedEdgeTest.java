71cbfe3ca3bf82fd799bd399290487be2afc71ad
Peter Siqueiros <pjs33@email.arizona.edu>
2017-05-31 16:34:34 -0700

    added test skeleton

--- src/tests/WeightedDirectedEdgeTest.java
+++ src/tests/WeightedDirectedEdgeTest.java
@@ -1,6 +1,26 @@
 package tests;
 
+import org.junit.Test;
+
 public class WeightedDirectedEdgeTest {
 	
+	@Test
+	private void testConstructor() {
 
+	}
+	
+	@Test
+	private void testFromVertexGetSet() {
+
+	}
+	
+	@Test
+	private void testToVertexGetSet() {
+
+	}
+	
+	@Test
+	private void testWeightGetSet() {
+		
+	}
 }
